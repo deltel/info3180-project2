4d079bfd782f6c4b59bc991dbc96514fd874bb0b
deltel <deltel000@gmail.com>
2020-04-27 23:47:18 -0500

    Added csrf stuff to init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 app.config['UPLOAD_FOLDER'] = './app/static/upload'
 app.config['SECRET_KEY'] = 'njsdbapdmakdoruer389r9ejfdnfio'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://project2:password007@localhost/db_project2'
