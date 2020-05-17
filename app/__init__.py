36991ae783d7c67ecd31f05c2d82757d1f0510b5
deltel <deltel000@gmail.com>
2020-05-17 18:50:19 -0500

    trying to merge

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,8 @@ csrf = CSRFProtect(app)
 app.config['UPLOAD_FOLDER'] = './app/static/upload'
 app.config['SECRET_KEY'] = 'iejf9003r-t4jf8jdm3201idjfnasd'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://project2:password007@localhost/db_project2'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password007@localhost/db_project2'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
