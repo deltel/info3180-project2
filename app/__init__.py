6bdffd30bdafd65a2f57fd302e4ab53378c362b9
deltel <deltel000@gmail.com>
2020-05-20 22:36:59 -0500

    Heroku setup

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,9 @@ csrf = CSRFProtect(app)
 #auth = HTTPBasicAuth()
 app.config['UPLOAD_FOLDER'] = './app/static/upload'
 app.config['SECRET_KEY'] = 'iejf9003r-t4jf8jdm3201idjfnasd'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://project2:password007@localhost/db_project2'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password007@localhost/db_project2'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://project2:password007@localhost/db_project2'
+#heroku
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bymnbialjmzwwm:45572583fc68fde1c2e1237f6f132f0361344de44f6753a844d19c2b10a7ce05@ec2-54-81-37-115.compute-1.amazonaws.com:5432/dbsvfv2m0vur3r'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
